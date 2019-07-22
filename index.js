bbff2512c7b95b21b27144dfc1a7966fd004fe71
Tracy <nguyentracy@outlook.com>
2019-07-22 18:01:27 -0400

    Add search bar functionality

--- index.js
+++ index.js
@@ -15,49 +15,92 @@ use the existing "avatar" code to add employees to the screen
 
 */
 
+function pineCone(userArray) {
+
+    const newArray = userArray.map( (user) => {
+        return {
+            picture : user.picture.large || user.picture.medium || user.picture.thumbmail,
+            name : `${user.name.first} ${user.name.last}`,
+            location : `${user.location.street} 
+            ${user.location.city} ${user.location.state}
+            ${user.location.postcode}`,
+            email : `${user.email}`,
+            phone : `${user.phone}`
+        }
+    });
+
+    container.innerHTML = " "
+    // Destructuring: 
+    newArray.forEach( ({picture, name, location, email, phone}) => {
+        const avatar = document.createElement('section');
+        avatar.classList.add('avatar')
+        avatar.innerHTML = `<div class="avatar-image">
+            <img src="${picture}" alt="${name}"/>
+        </div>
+        <div class="avatar-content">
+            <h2 class="avatar-header">${name}</h2>
+            <div class="avatar-location">
+                ${location}
+            </div>
+            <ul class="avatar-contact-list">
+                <li class="avatar-contact-list-item">
+                    <a href="mailto:${email}">✉</a>
+                </li>
+                <li class="avatar-contact-list-item">
+                    <a href="tel:${phone}">✆</a> 
+                </li>
+            </ul>
+        </div>`;
+    container.appendChild(avatar)
+    })
+
+}
+
+function pineappleBear(userData) { 
+    const search = document.querySelector('.search')
+    search.style.display ='flex'
+    search.style.height = "auto"
+
+    const searchBarList = document.createElement('ol')
+    searchBarList.style.listStyle = 'none'
+    searchBarList.style.display ='flex'
+
+    searchBarList.style.padding="0px"
+    searchBarList.style.justifyContent ="space-around"
+    searchBarList.style.alignItems="center"
+    searchBarList.style.width ="100vw"
+    search.appendChild(searchBarList)
+
+    const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    letters.forEach( letter => {
+        const searchBarLetters = document.createElement('li')
+        searchBarLetters.style.border ="2px solid white"
+        searchBarLetters.style.padding ="15px"
+        searchBarLetters.innerHTML = letter
+        searchBarList.appendChild(searchBarLetters)
+    })
+
+    // When user clicks on letter
+    search.addEventListener('click', event =>  {
+        if (event.target !== event.currentTarget) {
+            console.log(event.target.innerHTML)
+            const letter = event.target.innerHTML
+            const filtered = userData.results.filter((user) => {
+                return user.name.last[0] === letter.toLowerCase()
+            })
+            console.log(filtered)
+            pineCone(filtered)
+        }
+
+    })
+
+}
 
 const container = document.querySelector('.container');
 fetch('https://randomuser.me/api/?results=50')
 .then(res => res.json())
 .then (res => {
     userData = (res)
-
-const newArray = userData.results.map( (user) => {
-    return {
-        picture : user.picture.large || user.picture.medium || user.picture.thumbmail,
-        name : `${user.name.first} ${user.name.last}`,
-        location : `${user.location.street} 
-        ${user.location.city} ${user.location.state}
-        ${user.location.postcode}`,
-        email : `${user.email}`,
-        phone : `${user.phone}`
-    }
-    
-});
-
-// Destructuring: 
-newArray.forEach( ({picture, name, location, email, phone}) => {
-    const avatar = document.createElement('section');
-    avatar.classList.add('avatar')
-    avatar.innerHTML = `<div class="avatar-image">
-        <img src="${picture}" alt="${name}"/>
-    </div>
-    <div class="avatar-content">
-        <h2 class="avatar-header">${name}</h2>
-        <div class="avatar-location">
-            ${location}
-        </div>
-        <ul class="avatar-contact-list">
-            <li class="avatar-contact-list-item">
-                <a href="mailto:${email}">✉</a>
-            </li>
-            <li class="avatar-contact-list-item">
-                <a href="tel:${phone}">✆</a> 
-            </li>
-        </ul>
-    </div>`;
-
-container.appendChild(avatar)
-})
-
+    pineappleBear(userData)
+    pineCone(userData.results)
 })

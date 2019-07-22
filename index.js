7d8d5c7b110f25314dad249c092049184cfbb58d
Jonathan <jonathan@boldideas.net>
2019-07-22 15:57:13 -0400

    used fetch to get api data

--- index.js
+++ index.js
@@ -2,21 +2,16 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log(`It's better to burn out than to fade away.`);
 });
 
-// let picture = userData.results[0].picture;
-// // console.log(picture)
-// // let name = userData.results[0].name
-// // console.log(name.first)
-// let location1 = userData.results[0].location
-// // console.log(userData.results[0].location.street)
-// let email = userData.results[0].email
-// // console.log(userData.results[0].email)
-// let telephone = userData.results[0].phone
-// // console.log(userData.results[0].phone)
+const container = document.querySelector('.container')
+
+fetch('https://randomuser.me/api?results=50')
+.then(res => res.json())
+.then (res => {
+   userData = (res)
 
 const infoArray = userData.results.map ( (user) => {
     return {
-        picture :`${user.picture.thumbnail}`,
-        // console.log(picture.thumbnail)
+        picture :`${user.picture.large}`,
         name: `${user.name.first} ${user.name.last}`,
         location1:`${user.location.street} 
         ${user.location.city} ${user.location.state} 
@@ -24,33 +19,85 @@ const infoArray = userData.results.map ( (user) => {
         email : `${user.email}`,
         telephone :`${user.phone}`
     }
+
 });
-// console.log(infoArray[1])
-const container = document.querySelector('.container')
+
+
 //destructuring:: 
 infoArray.forEach(({picture,name,location1,telephone, email}) => {
     const card = document.createElement('section')
     card.classList.add("avatar")
     card.innerHTML = `<div class="avatar-image">
     <img src="${picture}" alt="${name}"/>
-</div>
-<div class="avatar-content">
+    </div>
+    <div class="avatar-content">
     <h2 class="avatar-header">${name}</h2>
     <div class="avatar-location">
-        ${location1}
+    ${location1}
     </div>
     <ul class="avatar-contact-list">
-        <li class="avatar-contact-list-item">
-            <a href="mailto:${email}">✉</a>
-        </li>
-        <li class="avatar-contact-list-item">
-            <a href="tel:${telephone}">✆</a> 
-        </li>
+    <li class="avatar-contact-list-item">
+    <a href="mailto:${email}">✉</a>
+    </li>
+    <li class="avatar-contact-list-item">
+    <a href="tel:${telephone}">✆</a> 
+    </li>
     </ul>
-</div>`;
-container.appendChild(card)
+    </div>`;
+    container.appendChild(card)
 });
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const search = document.querySelector('.search');
+// search.addEventListener('click', ()=>{
+//     console.log('click')
+// });
+
+// const alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+// const order = document.createElement('ul')
+// order.style.display="flex"
+// order.style.listStyle = 'none'
+// search.style.justifyContent = 'space-evenly'
+// search.appendChild(order)
+// search.style.border = "1px solid white"
+// search.style.display = 'flex'
+
+// alphaArray = alpha.split("")
+// console.log(alphaArray)
+
+// alphaArray.forEach((i) => {
+//     const btn = document.createElement('li')
+//     btn.innerHTML = i
+//     order.appendChild(btn)
     
+// })
+
+
+
+
+
+
 
 /*
 

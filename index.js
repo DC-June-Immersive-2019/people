6719586d0b3b948e948d2ae586ca05c0746d8ae2
Nathan Evans <nathanevans777@gmail.com>
2019-07-22 21:17:56 -0400

    Here is my API integration for an employee directory

--- index.js
+++ index.js
@@ -1,16 +1,73 @@
+//formatting JS to match HTML currently in place
+const container = document.querySelector(`.container`);
+const searchCont = document.querySelector(`.searchContainer`)
+const searchbar = document.querySelector(`.search`);
+const submit = document.querySelector(`.submit`);
+const reset = document.querySelector(`.reset`);
+//variable to connect profiles and information
+let currentUsers;
+//gathering the proper info for creating profiles        
+const createPerson = (users) => {
+    const mappedUsers = users.map(user => {
+            return{
+            name:`${user.name.first} ${user.name.last}`,
+            location:`${user.location.street} <br>${user.location.city} <br>${user.location.state} <br>${user.location.postcode}`,
+            picture: user.picture.large|| user.picture.medium|| user.picture.thumbnail,
+            email: user.email,
+            phone:user.email,
+            }
+    })
+return mappedUsers
+}
+//implementing the gatehred information into proper HTML formatting
+const createProfile = (newUser) => {
+    container.innerHTML = ``
+    for (i=0; i<newUser.length; i++) {
+    const profile = document.createElement(`section`);
+    profile.classList.add('avatar')
+    profile.innerHTML=`     
+        <div class="avatar-image">
+        <img src="${newUser[i].picture}" alt=""/>
+        </div>
+        <div class="avatar-content">
+        <h2 class="avatar-header">${newUser[i].name}</h2>
+        <div class="avatar-location">
+            ${newUser[i].location}</div>
+        <ul class="avatar-contact-list">
+            <li class="avatar-contact-list-item">
+                <a href="${newUser[i].email}">✉</a>
+            </li>
+            <li class="avatar-contact-list-item">
+                <a href="${newUser[i].phone}">✆</a> 
+            </li>
+        </ul>
+        </div>
+        `;
+    container.appendChild(profile);
+    }
 
-document.addEventListener('DOMContentLoaded', () => {
-    console.log(`It's better to burn out than to fade away.`);
-});
-
-/*
-
-We are going to create an Employee address book.  We will
-use the existing "avatar" code to add employees to the screen
-
-1)  Update the JavaScript code to read all of the user data from
-    the people.js file and show all the users on the page
-2)  Add a filter to the data to only return users whose last
-    name start with a given letter
-
-*/
+}
+//page starter. Goes and gathers API content, forces it into functions
+const start = () => {
+    const container = document.querySelector(`.container`); 
+    fetch(`https://randomuser.me/api/?results=50`)
+    .then (res => res.json())
+    .then(res => {
+        currentUsers = createPerson(res.results)
+        createProfile(currentUsers)
+    })
+}
+//Event listener add to improve search effects
+searchCont.addEventListener('click', (e) =>{
+    if (e.target === submit) {
+        const searchParam = document.querySelector('.search').value;
+        const filteredUsers = currentUsers.filter(function (person) {
+            return person.name[0] === searchParam;
+        })
+        createProfile(filteredUsers)
+        } else {
+            (e.target === reset)
+            createProfile(currentUsers)
+    } 
+})
+document.addEventListener('DOMContentLoaded', start);
